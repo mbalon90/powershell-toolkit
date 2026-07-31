5c77d98a3dbb3f4fdfa3ca86ee664ec3e954afe1
Michal Balon <michal.balon.90@gmail.com>
2026-07-31 23:22:01 +0200

    0002

--- helpers.ps1
+++ helpers.ps1
@@ -1,30 +1,11 @@
+# This file contains helper functions for the PowerShell application. It includes functions for listing all main and helper functions along with their descriptions.
+
 <# .DESCRIPTION 
 Listing All Main Functions #>
 function Get-Functions {
     $tokens = $null
     $errors = $null
     $ast = [System.Management.Automation.Language.Parser]::ParseFile("$PSScriptRoot\functions.ps1", [ref]$tokens, [ref]$errors)
-
-    $functions = $ast.FindAll({
-            param($node)
-            $node -is [System.Management.Automation.Language.FunctionDefinitionAst]
-        }, $true)
-
-    foreach ($f in $functions) {
-        $help = $f.GetHelpContent()
-        $description = if ($help -and $help.Description) { $help.Description.Trim() } else { '' }
-        [pscustomobject]@{
-            Name        = $f.Name
-            Description = if ($description.Length -gt 50) { $description.Substring(0, 47) + '...' } else { $description }
-        }
-    }
-}
-<# .DESCRIPTION 
-Listing All Helper Functions #>
-function Get-Helpers {
-    $tokens = $null
-    $errors = $null
-    $ast = [System.Management.Automation.Language.Parser]::ParseFile("$PSScriptRoot\helpers.ps1", [ref]$tokens, [ref]$errors)
 
     $functions = $ast.FindAll({
             param($node)
