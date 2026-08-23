cb423a1ae79486b82a8f2d1fac45aa55dc4aae7d
Michal Balon <michal.balon.90@gmail.com>
2026-08-24 00:20:47 +0200

    clean up synopsis messages

--- scripts/HealthCheck.ps1
+++ scripts/HealthCheck.ps1
@@ -1,16 +1,16 @@
-<# .SYNOPSIS
+function Get-HealthCheckBanner { 
+    <# .SYNOPSIS
     This function displays the health check banner.
 #>
 <# .DESCRIPTION
     This function displays the health check banner.
 #>
-function Get-HealthCheckBanner { 
     Write-Host "==========================================" -ForegroundColor Green
     Write-Host "          System Health Check             " -ForegroundColor Green
     Write-Host "==========================================" -ForegroundColor Green
 }
-
-<# .SYNOPSIS
+function Get-DiskHealth {
+    <# .SYNOPSIS
     This script performs a health check on the system, including disk space, RAM usage, running services, Windows update status, and event log errors. 
     The results are exported to a CSV file.
 #>
@@ -18,7 +18,6 @@ function Get-HealthCheckBanner {
     The script checks the health of the system by performing various checks and exporting the results to a CSV file. 
     It checks disk space, RAM usage, running services, Windows update status, and event log errors.
 #>
-function Get-DiskHealth {
     $fixedDrives = [System.IO.DriveInfo]::GetDrives() | Where-Object { $_.IsReady -eq $true -and $_.DriveType -eq "Fixed" } 
     $diskHealth = foreach ($drive in $fixedDrives) {    
         $percentFreeSpace = [math]::Round(($drive.TotalFreeSpace / $drive.TotalSize) * 100, 1)    
@@ -44,14 +43,13 @@ function Get-DiskHealth {
     }
     $diskHealth 
 }
-
-<# .SYNOPSIS
+function Get-RAMHealth {
+    <# .SYNOPSIS
     This function checks the health of the system's RAM.
 #>
 <# .DESCRIPTION
     This function checks the health of the system's RAM by evaluating the percentage of memory used.
 #>
-function Get-RAMHealth {
 
 }
 
